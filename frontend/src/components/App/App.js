fd30f954dcc7261542be2945e40e31f696e90a7d
“Erin <erinkelley27@gmail.com>
2018-08-16 14:45:46 -0400

    adjusted App.js path in index.js

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-
+      <h1>test</h1>
     )
   }
 }
