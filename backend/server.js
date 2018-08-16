24a4c8d31f9a5f5210f242d15c5ef8a7ff2a420d
“Erin <erinkelley27@gmail.com>
2018-08-16 11:38:55 -0400

    set up express and server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const parser = require('body-parser')
+
+const app = express()
+
+const userController = require('./controllers/user.js')
+
+app.use(parser.json())
+
+app.use('/users', userController)
+
+app.listen(3001, () => console.log('Listening on port 3001 :)'))
