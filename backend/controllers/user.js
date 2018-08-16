032fde0e46216b917530fa1461eac41426928aa3
“Erin <erinkelley27@gmail.com>
2018-08-16 13:59:52 -0400

    requirements in user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jwt-simple')
+const passport = require('../config/passport')
+const config = require('../config/config')
+const mongoose = require('../models/User')
+const User = mongoose.model('User')
 
 module.exports = router
