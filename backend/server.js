76bf6724e899e7c4d1248ed3fa0e5628052a8dcc
“Erin <erinkelley27@gmail.com>
2018-08-16 13:56:05 -0400

    required user controller in server

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ const passport = require('./config/passport')()
 const app = express()
 
 const postcardController = require('./controllers/postcard.js')
+const userController = require('./controllers.user.js')
 
 app.use(parser.json())
 app.use(passport.initialize())
 
 app.use('/', postcardController)
+app.use('/user', userController)
 
 app.listen(3001, () => console.log('Listening on port 3001 :)'))
