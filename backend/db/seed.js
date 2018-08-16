241bdf97b85bf4b208cd2638243e86b292811e81
“Erin <erinkelley27@gmail.com>
2018-08-16 12:26:41 -0400

    added postcard data

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -1,13 +1,27 @@
 const mongoose = require('../models/User')
-const userdata = require('./userdata')
+const userData = require('./userdata')
+const postcardData = require('./postcarddata')
 
 const User = mongoose.model('User')
+const Postcard = mongoose.model('Postcard')
 
 User.remove({})
   .then(_ => {
-    User.collection.insert(userdata)
+    User.collection.insert(userData)
       .then(users => {
-        console.log(users)
+        console.log(userData)
+        process.exit()
+      })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+Postcard.remove({})
+  .then(_ => {
+    Postcard.collection.insert(postcardData)
+      .then(postcards => {
+        console.log(postcardData)
         process.exit()
       })
   })
